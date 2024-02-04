e9196d9566f0fee162e150e5021d90ab50863574
Giovanni Ramos <krkbjj@gmail.com>
2024-02-03 22:30:07 -0300

    feat(libs): install libs

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ActivityIndicator, StyleSheet, Platform } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'; // Import SafeAreaView from 'react-native-safe-area-context'
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import theme from '@theme/index';
+
+// If you are using StatusBar from 'react-native', keep this import.
+import { StatusBar } from 'react-native'; 
+import { Routes } from '@routes/index';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={globalStyles(theme).container}>
+      <ThemeProvider theme={theme}>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor={theme.COLORS.GRAY_700}
+        />
+        {fontsLoaded 
+          ? <Routes /> 
+          : <ActivityIndicator size="large" /> // Optionally specify size
+        }
+      </ThemeProvider>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
+const globalStyles = (theme) => StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.COLORS.GRAY_700,
+    paddingTop: Platform.OS === 'android' ? 8 : 0,
+    paddingHorizontal: 16
   },
 });
